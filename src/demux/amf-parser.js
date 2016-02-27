4362cd899851e410c15e6e1a997247a8fccffa2f
xqq <xqq@xqq.im>
2016-02-27 16:10:15 +0800

    demux: Add AMF parser for flv metadata parsing

---
+++ src/demux/amf-parser.js
@@ -0,0 +1,198 @@
+import decodeUTF8 from '../utils/utf8-conv.js';
+
+function Swap16(src) {
+    return (((src >>> 8) & 0xFF) |
+            ((src & 0xFF) << 8));
+}
+
+function Swap32(src) {
+    return (((src & 0xFF000000) >>> 24) |
+            ((src & 0x00FF0000) >>> 8)  |
+            ((src & 0x0000FF00) << 8)   |
+            ((src & 0x000000FF) << 24));
+}
+
+let le = (function () {
+    let buf = new ArrayBuffer(2);
+    (new DataView(buf)).setInt16(0, 256, true);  // little-endian write
+    return (new Int16Array(buf))[0] === 256;  // platform-spec read, if equal then LE
+})();
+
+class AMF {
+
+    static parseScriptData(arrayBuffer, dataOffset, dataSize) {
+        let data = {};
+
+        try {
+            let name = AMF.parseValue(arrayBuffer, dataOffset, dataSize);
+            let value = AMF.parseValue(arrayBuffer, dataOffset + name.size, dataSize - name.size);
+
+            data[name.data] = value.data;
+        } catch (e) {
+            console.error(e);
+        }
+
+        return data;
+    }
+
+    static parseObject(arrayBuffer, dataOffset, dataSize) {
+        if (dataSize < 3) {
+            throw 'AMF: data not enough when parse ScriptDataObject';
+        }
+        let name = AMF.parseString(arrayBuffer, dataOffset, dataSize);
+        let value = AMF.parseValue(arrayBuffer, dataOffset + name.size, dataSize - name.size);
+
+        return {
+            data: {
+                name: name.data,
+                value: value.data
+            },
+            size: name.size + value.size
+        };
+    }
+
+    static parseVariable(arrayBuffer, dataOffset, dataSize) {
+        return AMF.parseObject(arrayBuffer, dataOffset, dataSize);
+    }
+
+    static parseString(arrayBuffer, dataOffset, dataSize) {
+        if (dataSize < 2) {
+            throw 'AMF: data not enough when parse String';
+        }
+        let v = new DataView(arrayBuffer, dataOffset, dataSize);
+        let length = Swap16(v.getUint16(0, le));
+
+        return {
+            data: decodeUTF8(new Uint8Array(arrayBuffer, dataOffset + 2, length)),
+            size: 2 + length
+        };
+    }
+
+    static parseLongString(arrayBuffer, dataOffset, dataSize) {
+        if (dataSize < 4) {
+            throw 'AMF: data not enough when parse LongString';
+        }
+        let v = new DataView(arrayBuffer, dataOffset, dataSize);
+        let length = Swap32(v.getUint32(0, le));
+
+        return {
+            data: decodeUTF8(new Uint8Array(arrayBuffer, dataOffset + 4, length)),
+            size: 4 + length
+        };
+    }
+
+    static parseDate(arrayBuffer, dataOffset, dataSize) {
+        if (dataSize !== 10) {
+            throw 'AMF: data size invalid when parse Date';
+        }
+        let v = new DataView(arrayBuffer, dataOffset, dataSize);
+        let timestamp = v.getFloat64(0, !le);
+        //let localTimeOffset = Swap16(v.getInt16(8, le));
+
+        return {
+            data: new Date(timestamp),
+            size: 8 + 2
+        };
+    }
+
+    static parseValue(arrayBuffer, dataOffset, dataSize) {
+        if (dataSize < 1) {
+            throw 'AMF: data not enough when parse Value';
+        }
+
+        let v = new DataView(arrayBuffer, dataOffset, dataSize);
+
+        let offset = 1;
+        let type = v.getUint8(0);
+        let value;
+
+        switch (type) {
+            case 0:  // Number(Double) type
+                value = v.getFloat64(1, !le);
+                offset += 8;
+                break;
+            case 1:  // Boolean type
+                let b = v.getUint8(1);
+                value = b ? true : false;
+                offset += 1;
+                break;
+            case 2:  // String type
+                let amfstr = AMF.parseString(arrayBuffer, dataOffset + 1, dataSize - 1);
+                value = amfstr.data;
+                offset += amfstr.size;
+                break;
+            case 3: { // Object(s) type
+                value = {};
+                let terminal = 0;  // workaround for malformed Objects which has missing ScriptDataObjectEnd
+                if ((Swap32(v.getUint32(dataSize - 4, le)) & 0x00FFFFFF) === 9) {
+                    terminal = 3;
+                }
+                while (offset < dataSize - 4) {  // 4 === type(UI8) + ScriptDataObjectEnd(UI24)
+                    let amfobj = AMF.parseObject(arrayBuffer, dataOffset + offset, dataSize - offset - terminal);
+                    value[amfobj.data.name] = amfobj.data.value;
+                    offset += amfobj.size;
+                }
+                if (offset <= dataSize - 3) {
+                    let marker = Swap32(v.getUint32(offset - 1, le)) & 0x00FFFFFF;
+                    if (marker === 9) {
+                        offset += 3;
+                    }
+                }
+                break;
+            }
+            case 8: { // ECMA array type (Mixed array)
+                value = {};
+                offset += 4;  // ECMAArrayLength(UI32)
+                let terminal = 0;  // workaround for malformed MixedArrays which has missing ScriptDataObjectEnd
+                if ((Swap32(v.getUint32(dataSize - 4, le)) & 0x00FFFFFF) === 9) {
+                    terminal = 3;
+                }
+                while (offset < dataSize - 8) {  // 8 === type(UI8) + ECMAArrayLength(UI32) + ScriptDataVariableEnd(UI24)
+                    let amfvar = AMF.parseVariable(arrayBuffer, dataOffset + offset, dataSize - offset - terminal);
+                    value[amfvar.data.name] = amfvar.data.value;
+                    offset += amfvar.size;
+                }
+                if (offset <= dataSize - 3) {
+                    let marker = Swap32(v.getUint32(offset - 1, le)) & 0x00FFFFFF;
+                    if (marker === 9) {
+                        offset += 3;
+                    }
+                }
+                break;
+            }
+            case 10:  // Strict array type
+                // ScriptDataValue[n]. NOTE: according to video_file_format_spec_v10_1.pdf
+                value = [];
+                let strictArrayLength = Swap32(v.getUint32(1, le));
+                offset += 4;
+                for (let i = 0; i < strictArrayLength; i++) {
+                    let val = AMF.parseValue(arrayBuffer, dataOffset + offset, dataSize - offset);
+                    value.push(val.data);
+                    offset += val.size;
+                }
+                break;
+            case 11:  // Date type
+                let date = AMF.parseDate(arrayBuffer, dataOffset + 1, dataSize - 1);
+                value = date.data;
+                offset += date.size;
+                break;
+            case 12:  // Long string type
+                let amfLongStr = AMF.parseString(arrayBuffer, dataOffset + 1, dataSize - 1);
+                value = amfLongStr.data;
+                offset += amfLongStr.size;
+                break;
+            default:
+                // ignore and skip
+                offset = dataSize;
+                console.log('AMF: Unsupported AMF value type ' + type);
+        }
+
+        return {
+            data: value,
+            size: offset
+        };
+    }
+
+}
+
+export default AMF;
---
+++ src/utils/utf8-conv.js
@@ -0,0 +1,67 @@
+// translate from C++ project libWinTF8 (https://github.com/m13253/libWinTF8)
+
+function checkContinuation(uint8array, start, checkLength) {
+    let array = uint8array;
+    if (start + checkLength < array.length) {
+        while (checkLength--) {
+            if ((array[++start] & 0xC0) !== 0x80)
+                return false;
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function decodeUTF8(uint8array) {
+    let out = [];
+    let input = uint8array;
+    let i = 0;
+    let length = uint8array.length;
+
+    while (i < length) {
+        if (input[i] < 0x80) {
+            out.push(String.fromCharCode(input[i]));
+            ++i;
+            continue;
+        } else if (input[i] < 0xC0) {
+            // fallthrough
+        } else if (input[i] < 0xE0) {
+            if (checkContinuation(input, i, 1)) {
+                let ucs4 = (input[i] & 0x1F) << 6 | (input[i + 1] & 0x3F);
+                if (ucs4 >= 0x80) {
+                    out.push(String.fromCharCode(ucs4 & 0xFFFF));
+                    i += 2;
+                    continue;
+                }
+            }
+        } else if (input[i] < 0xF0) {
+            if (checkContinuation(input, i, 2)) {
+                let ucs4 = (input[i] & 0xF) << 12 | (input[i + 1] & 0x3F) << 6 | input[i + 2] & 0x3F;
+                if (ucs4 >= 0x800 && (ucs4 & 0xF800) !== 0xD800) {
+                    out.push(String.fromCharCode(ucs4 & 0xFFFF));
+                    i += 3;
+                    continue;
+                }
+            }
+        } else if (input[i] < 0xF8) {
+            if (checkContinuation(input, i, 3)) {
+                let ucs4 = (input[i] & 0x7) << 18 | (input[i + 1] & 0x3F) << 12
+                         | (input[i + 2] & 0x3F) << 6 | (input[i + 3] & 0x3F);
+                if (ucs4 > 0x10000 && ucs4 < 0x110000) {
+                    ucs4 -= 0x10000;
+                    out.push(String.fromCharCode((ucs4 >>> 10) | 0xD800));
+                    out.push(String.fromCharCode((ucs4 & 0x3FF) | 0xDC00));
+                    i += 4;
+                    continue;
+                }
+            }
+        }
+        out.push(String.fromCharCode(0xFFFD));
+        ++i;
+    }
+
+    return out.join('');
+}
+
+export default decodeUTF8;

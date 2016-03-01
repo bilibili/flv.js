6bfa3d0b071ccc45175253e2444a85d9be89c056
xqq <xqq@xqq.im>
2016-03-01 15:53:56 +0800

    demux: Add H264 SPS Parser to extract info like width & height

---
+++ src/demux/sps-parser.js
@@ -0,0 +1,154 @@
+import ExpGolomb from './exp-golomb.js';
+
+class SPSParser {
+
+    // TODO: extract more info (such as chroma_format(420, 422, 444), avc_profile, fps, ...)
+    static parseSPS(uint8array) {
+        let gb = new ExpGolomb(uint8array);
+
+        gb.readByte();
+        let profile_idc = gb.readByte();  // profile_idc
+        gb.readByte();  // constraint_set_flags[5] + reserved_zero[3]
+        let level_idc = gb.readByte();  // level_idc
+        gb.readUEG();  // seq_parameter_set_id
+
+        let chroma_format_idc = 0;
+        if (profile_idc === 100 || profile_idc === 110 || profile_idc === 122 ||
+            profile_idc === 244 || profile_idc === 44 || profile_idc === 83 ||
+            profile_idc === 86 || profile_idc === 118 || profile_idc === 128 ||
+            profile_idc === 138 || profile_idc === 144) {
+
+            chroma_format_idc = gb.readUEG();
+            if (chroma_format_idc === 3) {
+                this.readBits(1);  // separate_colour_plane_flag
+            }
+            gb.readUEG();  // bit_depth_luma_minus8
+            gb.readUEG();  // bit_depth_chroma_minus8
+            gb.readBits(1);  // qpprime_y_zero_transform_bypass_flag
+            if (gb.readBool()) {  // seq_scaling_matrix_present_flag
+                let scaling_list_count = (chroma_format_idc !== 3) ? 8 : 12;
+                for (let i = 0; i < scaling_list_count; i++) {
+                    if (gb.readBool()) {  // seq_scaling_list_present_flag
+                        if (i < 6) {
+                            SPSParser._skipScalingList(gb, 16);
+                        } else {
+                            SPSParser._skipScalingList(gb, 64);
+                        }
+                    }
+                }
+            }
+        }
+        gb.readUEG();  // log2_max_frame_num_minus4
+        let pic_order_cnt_type = gb.readUEG();
+        if (pic_order_cnt_type === 0) {
+            gb.readUEG();  // log2_max_pic_order_cnt_lsb_minus_4
+        } else if (pic_order_cnt_type === 1) {
+            gb.readBits(1);  // delta_pic_order_always_zero_flag
+            gb.readSEG();  // offset_for_non_ref_pic
+            gb.readSEG();  // offset_for_top_to_bottom_field
+            let num_ref_frames_in_pic_order_cnt_cycle = gb.readUEG();
+            for (let i = 0; i < num_ref_frames_in_pic_order_cnt_cycle; i++) {
+                gb.readSEG();  // offset_for_ref_frame
+            }
+        }
+        gb.readUEG();  // max_num_ref_frames
+        gb.readBits(1);  // gaps_in_frame_num_value_allowed_flag
+
+        let pic_width_in_mbs_minus1 = gb.readUEG();
+        let pic_height_in_map_units_minus1 = gb.readUEG();
+
+        let frame_mbs_only_flag = gb.readBits(1);
+        if (frame_mbs_only_flag === 0) {
+            gb.readBits(1);  // mb_adaptive_frame_field_flag
+        }
+        gb.readBits(1);  // direct_8x8_inference_flag
+
+        let frame_crop_left_offset = 0;
+        let frame_crop_right_offset = 0;
+        let frame_crop_top_offset = 0;
+        let frame_crop_bottom_offset = 0;
+
+        let frame_cropping_flag = gb.readBool();
+        if (frame_cropping_flag) {
+            frame_crop_left_offset = gb.readUEG();
+            frame_crop_right_offset = gb.readUEG();
+            frame_crop_top_offset = gb.readUEG();
+            frame_crop_bottom_offset = gb.readUEG();
+        }
+
+        let sar_width = 1, sar_height = 1;
+        let vui_parameters_present_flag = gb.readBool();
+        if (vui_parameters_present_flag) {
+            if (gb.readBool()) {  // aspect_ratio_info_present_flag
+                let aspect_ratio_idc = gb.readByte();
+                let sar_w_table = [1, 12, 10, 16, 40, 24, 20, 32, 80, 18, 15, 64, 160, 4, 3, 2];
+                let sar_h_table = [1, 11, 11, 11, 33, 11, 11, 11, 33, 11, 11, 33,  99, 3, 2, 1];
+
+                if (aspect_ratio_idc > 0 && aspect_ratio_idc < 16) {
+                    sar_width = sar_w_table[aspect_ratio_idc - 1];
+                    sar_height = sar_h_table[aspect_ratio_idc - 1];
+                } else if (aspect_ratio_idc === 255) {
+                    sar_width = gb.readByte() << 8 | gb.readByte();
+                    sar_height = gb.readByte() << 8 | gb.readByte();
+                }
+            }
+        }
+
+        let sarScale = 1;
+        if (sar_width !== 1 || sar_height !== 1) {
+            sarScale = sar_width / sar_height;
+        }
+
+        let crop_unit_x = 0, crop_unit_y = 0;
+        if (chroma_format_idc === 0) {
+            crop_unit_x = 1;
+            crop_unit_y = 2 - frame_mbs_only_flag;
+        } else {
+            let sub_wc = (chroma_format_idc === 3) ? 1 : 2;
+            let sub_hc = (chroma_format_idc === 1) ? 2 : 1;
+            crop_unit_x = sub_wc;
+            crop_unit_y = sub_hc * (2 - frame_mbs_only_flag);
+        }
+
+        let codec_width = (pic_width_in_mbs_minus1 + 1) * 16;
+        let codec_height = (2 - frame_mbs_only_flag) * ((pic_height_in_map_units_minus1 + 1) * 16);
+
+        codec_width -= (frame_crop_left_offset + frame_crop_right_offset) * crop_unit_x;
+        codec_height -= (frame_crop_top_offset + frame_crop_bottom_offset) * crop_unit_y;
+
+        let present_width = Math.ceil(codec_width * sarScale);
+
+        gb.destroy();
+        gb = null;
+
+        return {
+            sarRatio: {
+                width: sar_width,
+                height: sar_height
+            },
+            codecSize: {
+                width: codec_width,
+                height: codec_height
+            },
+            presentSize: {
+                width: present_width,
+                height: codec_height
+            }
+        };
+    }
+
+    static _skipScalingList(gb, count) {
+        let last_scale = 8, next_scale = 8;
+        let delta_scale = 0;
+        for (let i = 0; i < count; i++) {
+            if (next_scale !== 0) {
+                delta_scale = gb.readSEG();
+                next_scale = (last_scale + delta_scale + 256) % 256;
+            }
+            last_scale = (next_scale === 0) ? last_scale : next_scale;
+        }
+    }
+
+}
+
+export default SPSParser;

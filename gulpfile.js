3dd85841cac3968fc436a300fdd7c6000b398489
xqq <xqq@xqq.im>
2016-03-06 09:19:26 +0800

    Build system: Switched to gulp + babel6

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var del = require('del');
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
+
+gulp.task('default', ['clean', 'build', 'minimize']);
+
+gulp.task('clean', function() {
+    return del([
+        'dist/*.*'
+    ]);
+});
+
+gulp.task('build', ['clean'], function() {
+    var b = browserify({
+        entries: 'src/flv.js',
+        debug: true,
+        transform: [babelify]
+    });
+
+    return b.bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('flv.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('minimize', ['build'], function() {
+    var options = {
+        sourceMap: true,
+        sourceMapIncludeSources: true,
+        sourceMapRoot: './src/',
+        mangle: true,
+        compress: {
+            sequences: true,
+            dead_code: true,
+            conditionals: true,
+            booleans: true,
+            unused: true,
+            if_return: true,
+            join_vars: true
+        }
+    };
+
+    return gulp.src('dist/flv.js')
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(uglify(options))
+            .on('error', console.error.bind(console))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist/'))
+});

c69125adb05365c43e0ac86167dd6e6769d43b4f
xqq <xqq@xqq.im>
2016-05-03 16:43:08 +0800

    Polyfill: Add ES6 Object.assign polyfill

--- src/utils/polyfill.js
+++ src/utils/polyfill.js
@@ -7,6 +7,26 @@ class Polyfill {
             return obj;
         };
 
+        // ES6 Object.assign
+        Object.assign = Object.assign || function (target) {
+            if (target === undefined || target === null) {
+                throw new TypeError('Cannot convert undefined or null to object');
+            }
+
+            let output = Object(target);
+            for (let i = 1; i < arguments.length; i++) {
+                let source = arguments[i];
+                if (source !== undefined && source !== null) {
+                    for (let key in source) {
+                        if (source.hasOwnProperty(key)) {
+                            output[key] = source[key];
+                        }
+                    }
+                }
+            }
+            return output;
+        };
+
         // ES6 Promise (missing support in IE11)
         if (typeof self.Promise !== 'function') {
             require('es6-promise').polyfill();

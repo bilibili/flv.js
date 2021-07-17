a7393173709ab6132cd1ca08f9596258e3a95025
xqq <xqq@xqq.im>
2021-07-17 21:14:19 +0900

    Migrate build system to Webpack 5 + TypeScript compiler

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const webpack = require('webpack');
+const pkg = require('./package.json');
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+let config = {
+    entry: './src/index.js',
+    output: {
+        filename: 'flv.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'flvjs',
+        libraryTarget: 'umd'
+    },
+
+    devtool: 'source-map',
+
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+        fallback: {
+            fs: false,
+            path: false
+        }
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            __VERSION__: JSON.stringify(pkg.version)
+        })
+    ],
+
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+            })
+        ]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)$/,
+                use: 'ts-loader',
+                exclude: /node-modules/
+            },
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                use: 'source-map-loader'
+            }
+        ]
+    }
+};
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'production') {
+        config.output.filename = 'flv.min.js';
+    }
+
+    return config;
+};

32067abd5ce92da63835e40bd3f526317a7910e6
xqq <xqq@xqq.im>
2021-07-17 21:16:16 +0900

    src/flv.js: Adapt version tagging by webpack

--- src/flv.js
+++ src/flv.js
@@ -85,7 +85,7 @@ Object.defineProperty(flvjs, 'version', {
     enumerable: true,
     get: function () {
         // replaced by browserify-versionify transform
-        return '__VERSION__';
+        return __VERSION__;
     }
 });
 
